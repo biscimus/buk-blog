8dab62b541ddf4a0d9780ca398d623ad672d41ef
geonhoyun <geonhoyun@gmail.com>
2025-01-14 16:45:57 +0100

    change default font weight

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,5 +1,5 @@
 import { IBM_Plex_Mono, IBM_Plex_Serif, IBM_Plex_Sans_KR } from "next/font/google";
 
-export const mono = IBM_Plex_Mono({ weight: "400", subsets: ["latin"], display: "swap" });
-export const serif = IBM_Plex_Serif({ weight: "400", subsets: ["latin", "cyrillic"], display: "swap" });
+export const mono = IBM_Plex_Mono({ weight: "500", subsets: ["latin"], display: "swap" });
+export const serif = IBM_Plex_Serif({ weight: "300", subsets: ["latin", "cyrillic"], display: "swap" });
 export const korean = IBM_Plex_Sans_KR({ weight: "400", subsets: ["latin"], display: "swap" });
