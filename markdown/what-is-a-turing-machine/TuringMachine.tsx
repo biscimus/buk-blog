b9ef14b4f5fcd2b2a5e2aad0a953438320fc69c5
geonhoyun <geonhoyun@gmail.com>
2025-01-28 04:48:36 +0100

    fix tm bugs

--- markdown/what-is-a-turing-machine/TuringMachine.tsx
+++ markdown/what-is-a-turing-machine/TuringMachine.tsx
@@ -1,44 +1,57 @@
 "use client";
 
-import { Bad_Script } from "next/font/google";
-import { setPriority } from "os";
-import { Dispatch, MouseEventHandler, ReactEventHandler, SetStateAction, useEffect, useRef, useState } from "react";
-
-
-// TODO: Configure q1 as final state
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 const directions = ["L", "N", "R"] as const;
 const alphabet = ["0", "1", "B"] as const;
 
 type Direction = typeof directions[number];
 type Alphabet = typeof alphabet[number];
-type Transition = { state: number, alphabet: Alphabet, direction: Direction };
+// type Transition = { state: number, alphabet: Alphabet, direction: Direction };
+
+type Transition = {
+    [alphabet: string]: {
+        state: number;
+        alphabet: Alphabet;
+        direction: Direction; // You can adjust the possible directions
+    };
+
+};
 
 export default function TuringMachine() {
 
     const [states, setStates] = useState([0, 1]);
-    const [transitions, setTransitions] = useState<Transition[]>([{ state: 1, alphabet: "0", direction: "R" }, { state: 1, alphabet: "0", direction: "N" }]);
+    const [transitions, setTransitions] = useState<Transition[]>([
+        {
+            "0": { state: 1, alphabet: "0", direction: "R" },
+            "1": { state: 1, alphabet: "0", direction: "N" },
+            "B": { state: 1, alphabet: "0", direction: "R" }
+        }
+    ]);
 
     const [band, setBand] = useState(Array<string>(100).fill("B"))
     const [head, setHead] = useState(50);
 
+    const [input, setInput] = useState("");
+
     const [currentState, setCurrentState] = useState(0);
 
-    const [isPlaying, setIsPlaying] = useState(false);
-    const intervalRef = useRef<NodeJS.Timeout | null>(null);
-
-    const bandHalfWidth = 4;
-
+    const bandHalfWidth = 3;
     const leftLimit = Math.max(0, head - bandHalfWidth);
-    const rightLimit = Math.min(band.length - 1, head + bandHalfWidth + 1);
+    const rightLimit = Math.min(band.length, head + bandHalfWidth + 1);
 
     const visibleBand = band.slice(leftLimit, rightLimit);
 
-    console.log("Visilbe band: ", visibleBand);
-
     const onAddState = () => {
         setStates((prevStates) => [...prevStates, prevStates.length]);
-        setTransitions(prev => [...prev, { state: 1, alphabet: "B", direction: "N" }])
+        setTransitions(prev => ([
+            ...prev,
+            {
+                "0": { state: 1, alphabet: "0", direction: "R" },
+                "1": { state: 1, alphabet: "0", direction: "N" },
+                "B": { state: 1, alphabet: "0", direction: "R" }
+            }
+        ]))
     }
 
     const onRemoveState = () => {
@@ -48,14 +61,16 @@ export default function TuringMachine() {
     }
 
     const onPlayOneStep = () => {
-        const input = band[head];
-        const { state, alphabet, direction } = transitions[currentState];
 
-        console.log("New soll state: ", state);
+        if (head === 0 || head === states.length - 1) return;
+        const input = band[head];
+        const { state, alphabet, direction } = transitions[currentState][input];
+        if (state === states.length - 1) return;
+
 
         setBand(prevBand => prevBand.map((element, index) => (index !== head) ? element : alphabet));
 
-        setCurrentState(prev => state);
+        setCurrentState(state);
 
         if (direction === "L") {
             setHead(Math.max(0, head - 1));
@@ -64,29 +79,38 @@ export default function TuringMachine() {
         }
     }
 
+    const [isPlaying, setIsPlaying] = useState(false);
+
     useEffect(() => {
         if (isPlaying) {
             const interval = setInterval(() => {
                 onPlayOneStep();
-            }, 1000);
+            }, 700);
             return () => clearInterval(interval);
         }
     }, [isPlaying, onPlayOneStep]);
 
-    console.log("current head: ", head);
-    console.log("Current state: ", currentState);
-    console.log("States: ", states)
-    console.log("Transitions: ", transitions)
+    const onSetInput = () => {
+
+
+        setBand(() => {
+            const newBand = Array<string>(100).fill("B");
+            input.split("").forEach((char, index) => {
+                newBand[50 + index] = char; // Start inserting characters at the 50th index
+            });
+            return newBand;
+        });
+    }
 
     return (
-        <div className="flex gap-4">
-            <div className="flex flex-col justify-center items-center grow-[3] border-2 rounded-md border-slate-300">
-                <div className="flex p-8">
+        <div className="flex gap-4 justify-center">
+            <div className="flex flex-col justify-center items-center border-2 rounded-md border-slate-300 p-4 gap-4">
+                <div className="flex">
                     {visibleBand.map((element, index) => (
                         <div key={index} className="flex flex-col">
-                            <div className="text-xs text-center pb-1">{index + head - bandHalfWidth}</div>
+                            <div className="text-xs text-center pb-1">{index + leftLimit}</div>
                             <div
-                                className={`w-12 h-12 bg-slate-300 p-2 text-center text-black ${index === Math.floor(visibleBand.length / 2) ? 'border-4 border-green-500' : 'border-4 border-black'}`}
+                                className={`w-12 h-12 bg-slate-300 p-2 text-center text-black ${index + leftLimit === head ? 'border-4 border-green-500' : 'border-4 border-black'}`}
                                 key={index}
                             >
                                 {element}
@@ -94,6 +118,18 @@ export default function TuringMachine() {
                         </div>
                     ))}
                 </div>
+                <div className="flex gap-2 items-center">
+                    <input className="rounded-sm bg-black border-[1px] border-slate-200 text-center" placeholder="Input 0-1-string" value={input} onChange={e => {
+                        const newInput = e.target.value;
+                        if (/^[01]*$/.test(newInput)) {
+                            setInput(newInput);
+                        } else {
+                            alert("Invalid input: Only 0 and 1 are allowed.");
+                        }
+                    }}></input>
+                    <button onClick={onSetInput}>OK</button>
+                </div>
+
                 <div className="flex gap-2">
                     <button className="text-black bg-green-600 px-2 py-1 rounded-sm" onClick={onPlayOneStep}>Play one step</button>
                     <button
@@ -104,16 +140,16 @@ export default function TuringMachine() {
                     </button>
                 </div>
             </div>
-            <div className="flex flex-col items-center p-4 border-2 rounded-md border-slate-300 grow-[1] gap-4">
+            <div className="flex flex-col items-center p-4 border-2 rounded-md border-slate-300 gap-4">
                 <div className="flex flex-col gap-2">
                     <div className="text-center">States</div>
                     <div className="flex flex-wrap justify-center gap-1">
-                        <button className="text-black h-8 w-8 flex items-center justify-center rounded-md bg-green-500 hover:bg-green-700" onClick={onRemoveState}>
+                        <button className="text-black h-8 w-8 flex items-center justify-center rounded-sm bg-green-500 hover:bg-green-700" onClick={onRemoveState}>
                             -
                         </button>
-                        <div className="text-black min-h-8 min-w-8 px-2 flex items-center justify-center rounded-md bg-green-500">{states.length}</div>
+                        <div className="text-black min-h-8 min-w-8 px-2 flex items-center justify-center rounded-sm bg-green-500">{states.length}</div>
                         {/* {states.map(state => <State key={state} state={state} setStates={setStates} setTransitions={setTransitions} />)} */}
-                        <button className="text-black h-8 w-8 flex items-center justify-center rounded-md bg-green-500 hover:bg-green-700" onClick={onAddState}>
+                        <button className="text-black h-8 w-8 flex items-center justify-center rounded-sm bg-green-500 hover:bg-green-700" onClick={onAddState}>
                             +
                         </button>
                     </div>
@@ -138,49 +174,83 @@ function TransitionTable({
 }) {
 
     return (
-        <>
-            <div className="flex gap-1">
-                <div className="flex-1"></div>
+        <div className="text-xs">
+            <div className="flex pb-1">
+                <div className="w-[5%]" />
                 <div className="flex-1 text-center">0</div>
                 <div className="flex-1 text-center">1</div>
                 <div className="flex-1 text-center">B</div>
             </div>
-            <div className="flex flex-col max-h-48 gap-1 overflow-y-scroll">
-                {states.map((state) => (
-                    <div key={state} className="flex gap-1 items-center [&>select]:rounded-sm">
+            <hr />
+            <div className="flex flex-col max-h-48 gap-1 mt-1 overflow-y-scroll">
+                {transitions.map((transition, state) => (
+                    <div key={state} className="flex gap-1 items-center">
                         <div className="flex-1 text-center">q{state}</div>
-                        <select className="flex-1 bg-slate-600 p-1 text-center [&>option]:text-center" defaultValue={transitions[state].state} onChange={(e) => {
-                            setTransitions((prev) =>
-                                prev.map((transition, index) => index !== state ? transition : { ...transition, state: parseInt(e.target.value) })
-                            )
-                        }}>
-                            {states.map((value) => (
-                                <option key={value} value={value}>
-                                    q{value}
-                                </option>
-                            ))}
-                        </select>
-                        <select className="flex-1 bg-slate-600 p-1 text-center" defaultValue={transitions[state].alphabet} onChange={(e) => {
-                            setTransitions((prev) =>
-                                prev.map((transition, index) => index !== state ? transition : { ...transition, alphabet: e.target.value as Alphabet })
-                            )
-                        }}>
-                            <option value="0">0</option>
-                            <option value="1">1</option>
-                            <option value="B">B</option>
-                        </select>
-                        <select className="flex-1 bg-slate-600 p-1 text-center" defaultValue={transitions[state].direction} onChange={(e) => {
-                            setTransitions((prev) =>
-                                prev.map((transition, index) => index !== state ? transition : { ...transition, direction: e.target.value as Direction })
-                            )
-                        }}>
-                            <option value="L">L</option>
-                            <option value="N">N</option>
-                            <option value="R">R</option>
-                        </select>
+                        {alphabet.map(letter => <div className="flex">
+                            <select className="flex-1 bg-slate-600 p-1 text-center [&>option]:text-center" defaultValue={transitions[state][letter].state} onChange={(e) => {
+                                setTransitions((prev) =>
+                                    prev.map((transition, index) =>
+                                        index === state
+                                            ? {
+                                                ...transition,
+                                                [letter]: {
+                                                    ...transition[letter],
+                                                    state: parseInt(e.target.value),
+                                                },
+                                            }
+                                            : transition
+                                    )
+                                );
+                            }}>
+                                {states.map((value, index) => (
+                                    <option key={value} value={value}>
+                                        {index < states.length - 1 ? "q" + value : `q\u0305`}
+                                    </option>
+                                ))}
+                            </select>
+                            <select className="flex-1 bg-slate-600 p-1 text-center" defaultValue={transitions[state][letter].alphabet} onChange={(e) => {
+                                setTransitions((prev) =>
+                                    prev.map((transition, index) =>
+                                        index === state
+                                            ? {
+                                                ...transition,
+                                                [letter]: {
+                                                    ...transition[letter],
+                                                    alphabet: e.target.value as Alphabet,
+                                                },
+                                            }
+                                            : transition
+                                    )
+                                );
+                            }}>
+                                <option value="0">0</option>
+                                <option value="1">1</option>
+                                <option value="B">B</option>
+                            </select>
+                            <select className="flex-1 bg-slate-600 p-1 text-center" defaultValue={transitions[state][letter].direction} onChange={(e) => {
+                                setTransitions((prev) =>
+                                    prev.map((transition, index) =>
+                                        index === state
+                                            ? {
+                                                ...transition,
+                                                [letter]: {
+                                                    ...transition[letter],
+                                                    direction: e.target.value as Direction,
+                                                },
+                                            }
+                                            : transition
+                                    )
+                                );
+                            }}>
+                                <option value="L">L</option>
+                                <option value="N">N</option>
+                                <option value="R">R</option>
+                            </select>
+                        </div>
+                        )}
                     </div>
                 ))}
             </div>
-        </>
+        </div>
     );
 }
