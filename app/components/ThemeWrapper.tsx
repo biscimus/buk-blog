76c833568c6cdb82120128313d417f01e6925145
Geonho Yun <geonho.yun@sap.com>
2026-01-19 09:20:48 +0100

    Remove bottom margin

--- app/components/ThemeWrapper.tsx
+++ app/components/ThemeWrapper.tsx
@@ -6,7 +6,7 @@ export default function ThemeWrapper({ children }: { children: React.ReactNode }
   const { theme } = useTheme();
   
   return (
-    <div className={`mx-auto max-w-4xl py-12 px-12 mb-32 min-h-screen
+    <div className={`mx-auto max-w-4xl py-12 px-12 min-h-screen
       text-gray-800 dark:text-neutral-300 
       transition-colors duration-300`}>
       {children}
